9219e47a651af0a02c837bc9b3198cda7d82d2eb
hnsblnbrg <hansibrahimasi@gmail.com>
2021-02-07 13:37:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
+//express app
+const app = express();
+
+//connect to mongoDB
+const dbURI =
+  "mongodb+srv://hnsblnbrg:dbtest123@nodeblog.8lne1.mongodb.net/node?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    //listen for requests only after database is loaded
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => console.log(err));
+app.use(morgan("dev"));
+
+//register view engine [by default will look at views folder]
+app.set("view engine", "ejs");
+
+//middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+//base routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+//blog routes
+app.use("/blogs", blogRoutes);
+
+//404 page [always positioned at the bottom]
+app.use((req, res) => {
+  res.status(400).render("404", { title: "404" });
+});
